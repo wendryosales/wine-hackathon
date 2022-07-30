3c510a52ae6f01c3c4b38023192bcace2a3829c2
Wendryo <wendryo.sales@gmail.com>
2022-07-30 16:42:29 -0300

    feat: :sparkles: Page Structure

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,1 +1,27 @@
-// página
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import fetchWines from '../../redux/actions';
+
+function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    function getWines() {
+      dispatch(fetchWines());
+    }
+    getWines();
+  }, []);
+
+  return (
+    <div className="Home">
+      <header>header aqui</header>
+      <main>
+        <h1>banner/carousel aqui</h1>
+        <h2>lista de vinhos aqui</h2>
+      </main>
+      <footer>footer aqui</footer>
+    </div>
+  );
+}
+
+export default Home;
